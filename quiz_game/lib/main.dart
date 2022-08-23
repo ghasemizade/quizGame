b6e434177a19169b541f99b32b40dc4052f70889
Hosein <mrqasemizade@gmail.com>
2022-08-23 11:12:28 +0430

    Start quiz game project

---
+++ quiz_game/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Text('hosein'),
+        ),
+      ),
+    );
+  }
+}
